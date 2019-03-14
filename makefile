06479fb9ce39386edd66e61318af65aa7404c233
Kewth <Kewth.K.D@outlook.com>
2019-03-14 20:16:07 +0800

    更新架构

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-~/.local/bin/PyLuogu: PyLuogu.py source/
+~/.local/bin/PyLuogu: dist/PyLuogu.py source/
 	pip3 install ./ --user
-	cp PyLuogu.py ~/.local/bin/PyLuogu
+	cp dist/PyLuogu.py ~/.local/bin/PyLuogu
 	chmod +x ~/.local/bin/PyLuogu
--- source/__init__.py
+++ source/__init__.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+'PyLuogu 的框架'
 import argparse
 from PyLuogu import read_problem
 
 def init_args():
+    '初始化参数'
     parser = argparse.ArgumentParser(description='''
 I'm too lazy to write this.
                 ''')
     parser.add_argument('-p', '--problem', action='append',
-            help='Read a problem')
+                        help='Read a problem')
     return parser.parse_args()
 
 def main():
+    '运行 PyLuogu'
     args = init_args()
     if args.problem:
         read_problem.read(args.problem[0])
