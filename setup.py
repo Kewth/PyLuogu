706e34a43ce0449891580b2f8ba01dca0d7dca12
Kewth <Kewth.K.D@outlook.com>
2019-03-15 16:40:43 +0800

    setup 自动安装依赖

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version='0.9',
     package_dir={'PyLuogu': 'source'},
     packages=['PyLuogu'],
+    install_requires=[
+        'argparse',
+        'bs4',
+        'colorama',
+        'lxml',
+    ],
     )
