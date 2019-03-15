73a8c444e81877fd40647ce0bb3859366c11dd96
Kewth <Kewth.K.D@outlook.com>
2019-03-15 19:25:25 +0800

    弃用 urllib.request

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
         'bs4',
         'colorama',
         'lxml',
+        'requests',
     ],
     )
--- source/read_problem.py
+++ source/read_problem.py
@@ -1,14 +1,14 @@
 '获取指定题目页面'
-from urllib import request
 import sys
 from PyLuogu import _print
+from PyLuogu import _link
 
 def read(problem_id):
     '获取 [problem_id] 题目的信息'
-    html = request.urlopen( \
+    page = _link.get_page( \
             'https://www.luogu.org/problemnew/show/{}'.format( \
-            problem_id)).read().decode('utf-8')
-    _print.print_problem_html(html)
+            problem_id), None, None)
+    _print.print_problem_html(page.text)
 
 if __name__ == '__main__':
     try:
