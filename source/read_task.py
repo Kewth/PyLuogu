cc95c82bac6c734b579924d3cda5e89e7bbc0c97
Kewth <Kewth.K.D@outlook.com>
2019-03-15 16:07:40 +0800

    优化任务计划的输出

--- source/read_task.py
+++ source/read_task.py
@@ -1,17 +1,19 @@
 '获取任务计划'
 import sys
 import requests
+from bs4 import BeautifulSoup
+from PyLuogu import _print
 
 def read(cid, uid):
-    print(cid, uid)
     '打印任务计划，需要 cookies __client_id=[cid], _uid=[uid]'
-    page = requests.get('https://www.luogu.org/', headers={ \
+    headers = { \
             'user-agent': \
             'Mozilla/5.0 (X11; Linux x86_64) \
             AppleWebKit/537.36 (KHTML, like Gecko) \
             Chrome/72.0.3626.121 Safari/537.36', \
             'cookie': \
-            '__client_id={}; _uid={}'.format(cid, uid)})
+            '__client_id={}; _uid={}'.format(cid, uid)}
+    page = requests.get('https://www.luogu.org/', headers=headers)
     # XXX
     html = page.text
     print('你的任务计划：')
@@ -23,7 +25,11 @@ def read(cid, uid):
         begin = html[:end].rfind('data-pid="') + 10
         if begin > end:
             break
-        print(html[begin:end])
+        problem = requests.get( \
+                'https://www.luogu.org/problemnew/show/{}'.format( \
+                html[begin:end]), headers=headers)
+        tree = BeautifulSoup(problem.text, 'lxml')
+        _print.print_title(tree)
         html = html[end + 6:]
 
 if __name__ == '__main__':
